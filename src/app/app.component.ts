e9b6e1a4423f020b29377e656ad32e7bba279bb0
lioneltraore <lioneltraore@bitbucket.org>
2020-09-11 18:04:57 +0000

    add screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bin2dec';
+  number: string;
+  result: string;
+
+  getInput(): void {
+    if (!this.number) {
+      this.result = '';
+    } else if (this.number.length <= 8 && this.number.length >= 1) {
+      this.result = parseInt(this.number, 2).toString();
+    }
+    console.log(this.number);
+  }
 }
